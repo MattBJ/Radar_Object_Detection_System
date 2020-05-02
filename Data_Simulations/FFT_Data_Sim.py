4a199b01c9be4cd98dac4d4501bf58d26c908a5b
MattBJ <40750771+MattBJ@users.noreply.github.com>
2020-05-01 23:55:03 -0500

    Changed the windowing function
    Instead of taking every other 2048 samples, I just take the first 2048 samples, then shift it all by the loop until reaching the end (90000 samples total)

--- Data_Simulations/FFT_Data_Sim.py
+++ Data_Simulations/FFT_Data_Sim.py
@@ -35,7 +35,7 @@ def fft_and_conversion(sample_buffer,sample_size,sample_freq):
 
 	spectrum /= sample_size
 
-	print(spectrum.shape) # 2048
+	# print(spectrum.shape) # 2048
 
 	# print(spectrum)
 
@@ -46,22 +46,23 @@ def fft_and_conversion(sample_buffer,sample_size,sample_freq):
 	# window 10 bins at a time, add them up, and append them to an array
 	# Find maximum window'd element --> Use that for the estimation
 
-	x = len(spectrum) # 1024
-	estimation = []
-	for i in range(int(x-10)):
-		sumate = 0
-		for j in range(10):
-			sumate += spectrum[i + j]
-		estimation.append(sumate)
-	estimation = np.array(estimation)
+	# x = len(spectrum) # 1024
+	# estimation = []
+	# for i in range(int(x-10)):
+		# sumate = 0
+		# for j in range(10):
+			# sumate += spectrum[i + j]
+		# estimation.append(sumate)
+	# estimation = np.array(estimation)
 
 
-	elementMax = np.where(estimation == np.amax(estimation))
+	# elementMax = np.where(estimation == np.amax(estimation))
+	elementMax = np.where(spectrum == np.amax(spectrum))
 	elementMax = elementMax[0] # gives the index into this estimation
 
-	elementMax += 5 # roughly halfway between estimation bins
+	# elementMax += 5 # roughly halfway between estimation bins
 
-	print('Bin with highest magnitude: ',elementMax)
+	# print('Bin with highest magnitude: ',elementMax)
 
 	# Know which frequency is the highest
 
@@ -70,7 +71,7 @@ def fft_and_conversion(sample_buffer,sample_size,sample_freq):
 	del_f = elementMax * bin_size # element number * bin size
 	del_f -= (bin_size/2) # average of the bin === midway between bins
 
-	print('frequency component: ',del_f)
+	# print('frequency component: ',del_f)
 
 	# Convert it to position
 
@@ -80,7 +81,7 @@ def fft_and_conversion(sample_buffer,sample_size,sample_freq):
 
 	return distance
 
-def read_elements(buffer_file,it_offset,sample_size,sample_freq):
+def read_elements(buffer_file,it_offset,sample_size,sample_freq,dist_file):
 	# Take 'sample_size' number of floating point elements from the 'buffer_file', using offset 'iteration'
 	# Then call a nested function that calculates the fft of the temporary buffer
 	# Final return should be: iteration offset (previous iteration offset + sample_size), and position value
@@ -95,31 +96,52 @@ def read_elements(buffer_file,it_offset,sample_size,sample_freq):
 				temp_buffer.append(float(line))
 	temp_buffer = np.array(temp_buffer)
 	distance = fft_and_conversion(temp_buffer,sample_size,sample_freq)
-	it_offset += sample_size
+	
+	s = str(distance[0]) + '\n'
+	dist_file.write(s)
+	
+	it_offset += 1 #sample_size
+	# it_offset no longer just skips completely
+
 	file.close() # minimize the RAM usage as much as possible.. maybe it's bad to keep opening and closing as a large file anyway? Idk
-	return it_offset, distance
+	return it_offset#, distance
 
 file_1_name = "RX1_buffer.txt"
 
 N = 9e4
 
 sample_size = 2048
+# sample_size = 512
 
 sample_freq = 100000 # 100ksps
 
-pos_buffer = []
+# pos_buffer = []
 it_offset = 0
 
-print(int(N/sample_size))
+file_out_name = "RX1_DistBufOut.txt"
+file_out = open(file_out_name,'w')
 
-for i in range(int(N/sample_size)): # rounds down
+
+# print(int(N/sample_size))
+
+# for i in range(int(N/sample_size)): # rounds down
+	# print(i)
+	# it_offset, tmp_dist = read_elements(file_1_name,it_offset,sample_size,sample_freq)
+	# pos_buffer.append(tmp_dist)
+
+for i in range(int(N - sample_size)): # After waitig x samples, can start 'really' sampling
 	print(i)
-	it_offset, tmp_dist = read_elements(file_1_name,it_offset,sample_size,sample_freq)
-	pos_buffer.append(tmp_dist)
+	# it_offset, tmp_dist = read_elements(file_1_name,it_offset,sample_size,sample_freq,file_out_name)
+	it_offset = read_elements(file_1_name,it_offset,sample_size,sample_freq,file_out)
+	# pos_buffer.append(tmp_dist)	
 
-pos_buffer = np.array(pos_buffer)
+file_out.close()
 
-print('iteration offset: ',it_offset)
-print(pos_buffer)
+# pos_buffer = np.array(pos_buffer)
 
-pos_buffer = np.array(pos_buffer)
+# print('iteration offset: ',it_offset)
+# print(pos_buffer)
+
+# pos_buffer = np.array(pos_buffer)
+
+# Note: Will just create another text file to store the 'measured distance'
